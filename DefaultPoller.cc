316da7612f6b59a1db722fe7416241633a1fe1dc
aling <yi7ling@gmail.com>
2024-09-08 16:32:33 +0800

    add poller module

---
+++ DefaultPoller.cc
@@ -0,0 +1,21 @@
+#include "Poller.h"
+
+#include <stdlib.h>
+
+/**
+ * newDefaultPoller的实现为什么要单独创建一个文件？
+ *  因为newDefaultPoller 的函数实现中要去构建派生类实例，这需要引用派生类（Epoller）的头文件
+ *  如果放在Poller.cc中去实现，会出现 基类 引用 派生类 的情况，这种
+ *  设计显然不合理，所以单独创建一个文件（公共区域）来实现。
+ */
+Poller* Poller::newDefaultPoller(EventLoop* loop)
+{
+    if (::getenv("MUDUO_USE_POLL"))
+    {
+        return nullptr; // 生成poll实例
+    }
+    else
+    {
+        return nullptr; // 生成epoll实例
+    }
+}
---
+++ Poller.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "noncopyable.h"
+#include "Timestamp.h"
+
+#include <vector>
+#include <unordered_map>
+
+class Channel;
+class EventLoop;
+
+// IO多路复用模块核心，抽象类
+class Poller : noncopyable
+{
+public:
+    typedef std::vector<Channel*> ChannelList;
+
+    Poller(EventLoop* loop_);
+    virtual ~Poller() = default;
+
+    // 轮询IO事件，必须被loop线程调用
+    virtual Timestamp poll(int timeoutMs, ChannelList* activeChannels) = 0; // 纯虚函数
+    virtual void updateChannel(Channel *channel) = 0;
+    virtual void removeChannel(Channel *channel) = 0;
+
+    // 判断 channel是否在当前 poller中
+    virtual bool hasChannel(Channel* channel) const;
+
+    // eventloop通过该接口获取默认的IO复用对象
+    static Poller* newDefaultPoller(EventLoop *loop);
+protected:
+    typedef std::unordered_map<int, Channel*> ChannelMap;
+    ChannelMap channels_;
+private:
+    EventLoop* ownerloop_;// 定义poller所属的eventloop
+};
